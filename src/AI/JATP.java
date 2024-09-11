5c22756e0f2f8fb6e335bb9c6cf07fbdbb3dbc6d
Caleb P. Nwokocha <calebnwokocha@gmail.com>
2024-09-11 06:32:43 -0500

    test

--- src/AI/JATP.java
+++ src/AI/JATP.java
@@ -7,8 +7,22 @@ import java.util.Scanner;
 public class JATP {
     private final HashMap<Object, Object> memory = new HashMap<>();
 
+    public JATP() {
+        System.out.println("JAVA AUTOMATED THEOREM PROVER (JATP)");
+        System.out.println();
+        System.out.println("Creator: Caleb Princewill Nwokocha");
+        System.out.println();
+        System.out.println("Theorem: Every equation has a left object and a right object.");
+        System.out.println("Example 1: a is left object of a=b and b is its right object.");
+        System.out.println("Example 2: a is left object of a=a and a is its right object.");
+        System.out.println("Example 3: a+b is left object of a+b=b+a and b+a is its right object.");
+        System.out.println("Example 4: a+b is left object of a+b=a+b and a+b is its right object.");
+        System.out.println();
+    }
+
     public void input(Object leftObject, Object rightObject) {
         if (memory.containsKey(leftObject)) {
+            disprove(leftObject, rightObject);
             output(leftObject);
         } else {
             memory.put(leftObject, rightObject);
@@ -17,7 +31,7 @@ public class JATP {
     }
 
     private void output(Object leftObject) {
-        System.out.println("generated equation: " + leftObject + "=" + memory.get(leftObject)
+        System.out.println("equation generated: " + leftObject + "=" + memory.get(leftObject)
                 + "=" + memory.get(memory.get(leftObject)));
         System.out.println("memory map: " + memory);
     }
@@ -32,18 +46,14 @@ public class JATP {
         }
     }
 
+    private void disprove(Object leftObject, Object rightObject) {
+        if (memory.containsKey(leftObject) && !memory.get(leftObject).equals(rightObject)) {
+            System.out.println("contradiction detected: " + leftObject + "≠" + rightObject);
+        }
+    }
+
     public static void main(String[] args) {
         JATP JATP = new JATP();
-        System.out.println("JAVA AUTOMATED THEOREM PROVER (JATP)");
-        System.out.println();
-        System.out.println("Creator: Caleb Princewill Nwokocha");
-        System.out.println();
-        System.out.println("Theorem: Every equation has a left object and a right object.");
-        System.out.println("Example 1: a is left object of a=b and b is its right object.");
-        System.out.println("Example 2: a is left object of a=a and a is its right object.");
-        System.out.println("Example 3: a+b is left object of a+b=b+a and b+a is its right object.");
-        System.out.println("Example 4: a+b is left object of a+b=a+b and a+b is its right object.");
-        System.out.println();
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
